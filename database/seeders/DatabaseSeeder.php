ad4a757eaeccb09d90920bb09912f81656ca4216
Botir <tiboruz@gmail.com>
2025-07-16 18:18:45 +0500

    seeding user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::create([
+            'name'=>'Admin',
+            'email'=>'info@fresh.uz',
+            'password'=>Hash::make('fresh123456'),
+            'role'=>'admin'
+        ]);
     }
 }
