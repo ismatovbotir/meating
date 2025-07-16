f35677079df64ab18d3a53304694d5f26344a832
Botir <tiboruz@gmail.com>
2025-07-16 17:40:19 +0500

    secound ommit

---
+++ database/migrations/2025_07_14_104409_create_cut_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cut_products', function (Blueprint $table) {
+            $table->id();
+            
+            $table->foreignId('shop_id');
+            $table->foreignId('user_id');
+            
+            $table->foreignId('cut_id');
+            $table->foreignId('item_id');
+            
+            $table->foreignId('product_id');
+            $table->integer('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cut_products');
+    }
+};
